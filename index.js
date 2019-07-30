209d63a22b69abffc4acad63b7c857e3e6e4e5e5
K <kittipat_1413@hotmail.com>
2019-07-30 19:00:56 +0700

    delete test.js

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const data = require('./data.json');
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 })
+
+app.get('/books/:id',(req, res) => {
+  // if book.id === req.params.id 
+  // return book
+  res.json(data.find(book => book.id === req.params.id))
+})
+
 
 app.listen(3000, () => {
   console.log('Start server at port 3000.')
